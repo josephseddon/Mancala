15608d039900b8444556304ef8fe7523c527c659
ARhWalters <hylian-hi-ho@live.co.uk>
2018-04-25 15:05:30 +0100

    Add files via upload

--- src/mancala/FXMLHomePageController.java
+++ src/mancala/FXMLHomePageController.java
@@ -5,14 +5,14 @@
  */
 package mancala;
 
-import java.io.File;
 import java.io.IOException;
 import java.net.URL;
 import java.sql.Connection;
 import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
 import java.util.ResourceBundle;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -22,31 +22,30 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.stage.Stage;
-import javafx.scene.image.*;
 
 /**
  * FXML Controller class
  *
- * @author josephseddpm
+ * @author Neo
  */
 public class FXMLHomePageController implements Initializable {
 
     /**
      * Initializes the controller class.
      */
-   
-    @FXML 
-    private ImageView profilePicture;
-    @FXML 
-    private Button startgameButton;
-    @FXML
-    private Button profileButton;
-    @FXML 
-    private Button logoutButton;
     
     @FXML 
-    public void startgameButtonAction(ActionEvent event) throws IOException {
-         System.out.println("Start game button clicked");
+    private Button startnewgameButton;
+    
+    @FXML 
+    private Button startoldgameButton;
+    
+    @FXML 
+    private Button leaderboardButton;
+    
+    @FXML 
+    public void startnewgameButtonAction(ActionEvent event) throws IOException {
+         System.out.println("Start new game button clicked");
          Parent home_page_parent = FXMLLoader.load(getClass().getResource("BoardView.fxml"));
          Scene home_page_scene = new Scene(home_page_parent);
          Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -54,9 +53,17 @@ public class FXMLHomePageController implements Initializable {
          app_stage.show();
     }
     
-    @FXML 
-    public void profileButtonClick(ActionEvent event) throws IOException {
-         System.out.println("ViewProfile button clicked");
+    public void startoldgameButtonAction(ActionEvent event) throws IOException {
+         System.out.println("Start old game button clicked");
+         Parent home_page_parent = FXMLLoader.load(getClass().getResource("BoardViewOld.fxml"));
+         Scene home_page_scene = new Scene(home_page_parent);
+         Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+         app_stage.setScene(home_page_scene);
+         app_stage.show();
+    }
+    
+    public void profileButtonAction(ActionEvent event) throws IOException {
+         System.out.println("Opening User Profile");
          Parent home_page_parent = FXMLLoader.load(getClass().getResource("FXMLOwnUserProfile.fxml"));
          Scene home_page_scene = new Scene(home_page_parent);
          Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
@@ -64,46 +71,55 @@ public class FXMLHomePageController implements Initializable {
          app_stage.show();
     }
     
-    @FXML 
-    public void logoutButtonClick(ActionEvent event) throws IOException {
-         System.out.println("Logout button clicked");
-         Parent home_page_parent = FXMLLoader.load(getClass().getResource("FXMLLoginPage.fxml"));
+    public void leaderboardButtonAction(ActionEvent event) throws IOException {
+         System.out.println("Opening Leaderboard");
+         Parent home_page_parent = FXMLLoader.load(getClass().getResource("FXMLLeaderboard.fxml"));
          Scene home_page_scene = new Scene(home_page_parent);
          Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
          app_stage.setScene(home_page_scene);
          app_stage.show();
     }
     
+    public void gameHistoryButtonAction(ActionEvent event) throws IOException{
+    	System.out.println("Opening GameHistory");
+    	Parent home_page_parent = FXMLLoader.load(getClass().getResource("FXMLGameHistory.fxml"));
+    	Scene home_page_scene = new Scene(home_page_parent);
+    	Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        app_stage.setScene(home_page_scene);
+        app_stage.show();
+    }
+    public void logOutButtonAction(ActionEvent event){
+    	System.out.println("log out");
+    	try {
+			deleteCurrentUsers();
+			Parent home_page_parent = FXMLLoader.load(getClass().getResource("FXMLLoginPage.fxml"));
+        	Scene home_page_scene = new Scene(home_page_parent);
+        	Stage app_stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+            app_stage.setScene(home_page_scene);
+            app_stage.show();
+    	}catch(SQLException | IOException e) {
+    		System.err.println(e.getMessage());
+    	}
+    	
+    }
+    /**
+     * 
+     * @return true - drop successfully  false - fail to drop 
+     * @throws SQLException
+     */
+    public void deleteCurrentUsers() throws SQLException{
+    	Connection c = null;
+    	PreparedStatement ps = null;
+    	c = DriverManager.getConnection("jdbc:sqlite:mancala.db");
+    	c.setAutoCommit(false);
+    	ps = c.prepareStatement("delete from CurrentUser");
+    	ps.executeUpdate();
+    	c.commit();
+    	c.close();
+    }
     @Override
-    public void initialize(URL url, ResourceBundle rb) { 
-        Connection c;
-        Statement stmt1;
-        Statement stmt2;
-        try {
-            c = DriverManager.getConnection("jdbc:sqlite:mancala.db");
-            c.setAutoCommit(false);
-            
-            System.out.println("Opened database successfully");
-            stmt1 = c.createStatement();     
-            stmt2 = c.createStatement();   
-            ResultSet rscurrent = stmt1.executeQuery("SELECT * FROM CurrentUser WHERE PlayerNo= '1'");
-            System.out.println(rscurrent.getString("CurrentUsers"));
-            ResultSet rs = stmt2.executeQuery("SELECT * FROM User WHERE username =  '" + rscurrent.getString("CurrentUsers") + "'");
-            
-            File imageFile = new File("src//mancala//"+rs.getString("profileimage"));
-            Image profileImage = new Image(imageFile.toURI().toString());
-            profilePicture.setImage(profileImage);
-            System.out.println(rs.getString("profileimage"));
-            
-            rscurrent.close();
-            rs.close();
-            stmt1.close();
-            stmt2.close();
-            c.close();
-        } catch (Exception e) {
-            System.err.println(e.getClass().getName() + ": " + e.getMessage() );
-            System.exit(0);
-        }      
+    public void initialize(URL url, ResourceBundle rb) {
+        // TODO
     }    
     
 }
