64098da64eb5191bb8488174a7fff73bedd0d522
josephseddon <josephseddon@gmail.com>
2018-04-27 10:37:37 +0100

    Add files via upload

---
+++ FavoriteUser.java
@@ -0,0 +1,46 @@
+package mancala;
+
+public class FavoriteUser {
+	private String username;
+	private String firstname;
+	private String lname;
+	private String wins;
+	private String losses;
+        private String ratio;
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getFirstname() {
+		return firstname;
+	}
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+	public String getLname() {
+		return lname;
+	}
+	public void setLname(String lname) {
+		this.lname = lname;
+	}
+	public String getWins() {
+		return wins;
+	}
+	public void setWins(String wins) {
+		this.wins = wins;
+	}
+	public String getLosses() {
+		return losses;
+	}
+	public void setLosses(String losses) {
+		this.losses = losses;
+	}
+        public String getRatio() {
+		return ratio;
+	}
+	public void setRatio(String ratio) {
+		this.ratio = ratio;
+	}
+}
